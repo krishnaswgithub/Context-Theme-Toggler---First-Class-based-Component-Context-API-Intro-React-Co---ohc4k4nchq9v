43664c5210199e56037a60347e3ec8938419d345
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-05-27 20:48:58 +0530

    Update ThemeProvider.js

--- src/components/ThemeProvider.js
+++ src/components/ThemeProvider.js
@@ -1,13 +1,33 @@
-import React from 'react';
+// import React from 'react';
 
-const ThemeContext = React.createContext()
-const ThemeProvider = (props) =>{
+// const ThemeContext = React.createContext()
+// const ThemeProvider = (props) =>{
 
-    return (
-        <React.Fragment>
+//     return (
+//         <React.Fragment>
 
-        </React.Fragment>
-    )
-}
+//         </React.Fragment>
+//     )
+// }
 
-export {ThemeProvider,ThemeContext}
+// export {ThemeProvider,ThemeContext}
+
+import React, { createContext, useState } from 'react';
+
+export const ThemeContext = createContext();
+
+const ThemeProvider = (props) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
+
+export default ThemeProvider;
