e304b4a861a8bdab3ec61efe5f7b28220a033cc1
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-05-27 20:51:44 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,38 @@
-import React from 'react'
+// import React from 'react'
+// import '../styles/App.css';
+// import { ThemeProvider } from './ThemeProvider';
+// import { ThemeToggleButton } from './ThemeToggleButton';
+// import { Page } from './Page'
+// const App = () => {
+
+//   return (
+//     <div id="main">
+//       <ThemeProvider>
+//         <Page />
+//         <ThemeToggleButton />
+//       </ThemeProvider>
+//     </div>
+//   )
+// }
+
+
+// export default App;
+
+import React from 'react';
 import '../styles/App.css';
 import { ThemeProvider } from './ThemeProvider';
 import { ThemeToggleButton } from './ThemeToggleButton';
-import { Page } from './Page'
-const App = () => {
+import { Page } from './Page';
 
+const App = () => {
   return (
     <div id="main">
       <ThemeProvider>
         <Page />
-        <ThemeToggleButton />
       </ThemeProvider>
+      <ThemeToggleButton />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
